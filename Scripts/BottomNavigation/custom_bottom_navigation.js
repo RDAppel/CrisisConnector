c051599fd0ac8887085b3279f53172327b235fb2
Bhuwan Tiwari <bhuwantiwari2002@gmail.com>
2023-04-06 14:39:52 -0500

    Working on the nav bar

--- Scripts/BottomNavigation/custom_bottom_navigation.js
+++ Scripts/BottomNavigation/custom_bottom_navigation.js
@@ -1,19 +1,60 @@
-window.customElements.define('bottom-navigation', class extends HTMLElement
-{
-    constructor()
-    {
-        super(); //Calling the constructor of the HTMLElement class and not the Skills 
-        const bottom_navigation_tag = document.querySelector('bottom-navigation');
-        const unorderList_tag = document.createElement('ul');
-        const list_tag_home = document.createElement('li');
-        const list_tag_hotline = document.createElement('li');
-        list_tag_home.innerHTML= "Home";
-        list_tag_hotline.innerHTML= "Hotline";
-        // Appending
-        bottom_navigation_tag.appendChild(unorderList_tag);
-        unorderList_tag.appendChild(list_tag_home);
-        unorderList_tag.appendChild(list_tag_hotline);
 
+class BottomNavigation extends HTMLElement {
+  
+    constructor() {
+      super();
+      const nav_tag = document.createElement('nav');
+      const ancor_tag = document.createElement('a');
+      const ancor_tag1 = document.createElement('a');
+      ancor_tag.innerHTML = "Home";
+      ancor_tag1.innerHTML = "📞 Hotlines";
+      const shadowRoot = this.attachShadow({ mode: 'open' });
+      shadowRoot.innerHTML = `
+        <style>
+          *{
+            font-family: sans-serif;
+            margin: 0;
+            padding: 0;
+            box-sizing: border-box;
+          }
+          nav{
+            display: flex;
+            justify-content: space-around;
+            background: #8E2DE2;  /* fallback for old browsers */
+            background: -webkit-linear-gradient(to right, #4A00E0, #8E2DE2);  
+            background: linear-gradient(to right, #4A00E0, #8E2DE2);
+            opacity: 90%;
+            padding: 12px;
+            position: fixed;
+            bottom: 0;
+            width: 100%;
+          }
+  
+          .nav-item{
+            align-items: center;
+            text-decoration: none;
+            color: white;
+            padding: 12px;
+            justify-content: space-around;
+          }
+          
+          .
+
+        </style>
+        <nav>
+          <img src="../Assets/Icons/homeIcon.png"/>
+          <a class="nav-item" href="#">
+            ${ancor_tag.innerHTML}
+          </a>
+  
+          <a class="nav-item" href="#">
+            ${ancor_tag1.innerHTML}
+          </a>
+        </nav>
+      `;
 
     }
-});
+  }
+  
+  customElements.define('bottom-navigation', BottomNavigation);
+  
