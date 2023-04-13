f4b1772912eee9ce6efb9b7aafe0253327931b08
Bhuwan <bhuwantiwari2002@gmail.com>
2023-04-13 08:02:11 -0500

    Added some changes

--- Scripts/Pages/resources.js
+++ Scripts/Pages/resources.js
@@ -9,7 +9,7 @@
 // Reading the data from the server
 async function getHotlineData() {
     try {
-      const response = await fetch("https://raw.githubusercontent.com/BhuwanTiwari2002/CrisisConnector/master/Data/TestJSON/hotline.json");
+      const response = await fetch("https://raw.githubusercontent.com/BhuwanTiwari2002/CrisisConnector/BhuwanBranch/Data/TestJSON/resources.json");
       return await response.json();
     } catch (error) {
       console.error(error);
@@ -17,7 +17,8 @@ async function getHotlineData() {
 }
 
 function displayHotlines(data) {  
-      data?.forEach(({title, types}) => {
+       console.log(data);
+      data?.forEach(({category, types}) => {
       const hotlineList = document.createElement('div');
       hotlineList.style = "margin-top: 10px";
       hotlineList.className = "searchHotline";
@@ -30,7 +31,8 @@ function displayHotlines(data) {
       hotlineH2.innerHTML = title;
       hotlineList.appendChild(hotlineTitle);
       hotlineTitle.appendChild(hotlineH2);
-      hotlineTitle.appendChild(hotlineName); 
+      hotlineTitle.appendChild(hotlineName);
+      
       types.forEach(({name, link}) => {
         const hotlineLink = document.createElement('a');
         hotlineLink.innerHTML = name;
@@ -42,17 +44,17 @@ function displayHotlines(data) {
     });
   }
   
-  $(document).ready(function() {
-    $("#search").on("keyup", function() {
-      var searchText = $(this).val().toLowerCase();
-      $(".searchHotline").find("h2, p").filter(function() {
-        return $(this).text().toLowerCase().indexOf(searchText) > -1;
-      }).show();
-      $(".searchHotline").find("h2, p").filter(function() {
-        return $(this).text().toLowerCase().indexOf(searchText) === -1;
-      }).hide();
-    });
-  });
+//   $(document).ready(function() {
+//     $("#search").on("keyup", function() {
+//       var searchText = $(this).val().toLowerCase();
+//       $(".searchHotline").find("h2, p").filter(function() {
+//         return $(this).text().toLowerCase().indexOf(searchText) > -1;
+//       }).show();
+//       $(".searchHotline").find("h2, p").filter(function() {
+//         return $(this).text().toLowerCase().indexOf(searchText) === -1;
+//       }).hide();
+//     });
+//   });
   
 //Hello this is a test
  
