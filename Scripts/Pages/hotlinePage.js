be0cf999fc6ec01e057cb0039de1c8489afd80b8
Ryan Appel <appel@fvtc.edu>
2023-04-14 12:02:18 -0500

    removed comments

--- Scripts/Pages/hotlinePage.js
+++ Scripts/Pages/hotlinePage.js
@@ -1,7 +1,6 @@
 
 (async () => displayHotlines(await getHotlineData()))();
 
-// Reading the data from the server
 const getHotlineData = async () => {
 	try {
 		const url = 'https://raw.githubusercontent.com/BhuwanTiwari2002/CrisisConnector/master/Data/TestJSON/hotline.json'
@@ -25,63 +24,20 @@ const displayHotlines = data => {
 	}
 
 	data.forEach(({ title, types }) => {
-		// const hotlineList = document.createElement('div');
-		// document.body.appendChild(hotlineList);
-		// hotlineList.classList.add('searchHotline');
-		// hotlineList.id = "hotlineList";
-		// //hotlineList.className = "searchHotline";
-		// // hotlineList.style = "margin-top: 10px"; // todo: move to css
 		createElementAndAppend(document.body, 'div', 'hotlineList', null, [ 'searchHotline' ]);
-
-		// const hotlineTitle = document.createElement('div');
-		// hotlineList.appendChild(hotlineTitle);
-		// hotlineTitle.id = "hotlineTitle";
 		createElementAndAppend(hotlineList, 'div', 'hotlineTitle')
-
-		// const hotlineH2 = document.createElement('h2');
-		// hotlineTitle.appendChild(hotlineH2);
-		// hotlineH2.innerText = title; // use innerText instead of innerHTML
 		createElementAndAppend(hotlineTitle, 'h2', null, title)
-
-		// const hotlineName = document.createElement('div');
-		// hotlineTitle.appendChild(hotlineName);
-		// hotlineName.id = "hotlineName";
 		createElementAndAppend(hotlineTitle, 'div', 'hotlineName')
-
-		//types.forEach(({ name, link }) => {
-			// const hotlineLink = document.createElement('a');
-			// hotlineName.appendChild(hotlineLink);
-			// hotlineLink.innerText = name;
-			// hotlineLink.href = link;
-			// //hotlineLink.target = '_blank'; // avoid using target="_blank" for pages on the same domain
-		//});
-
 		types.forEach(({ name, link }) => {
 			createElementAndAppend(hotlineName, 'a', null, name).href = link;
 		});
 	});
 }
 
-//   $(document).ready(function() {
-//     $("#search").on("keyup", function() {
-//       var searchText = $(this).val().toLowerCase();
-//       $(".searchHotline").find("h2, p").filter(function() {
-//         return $(this).text().toLowerCase().indexOf(searchText) > -1;
-//       }).show();
-//       $(".searchHotline").find("h2, p").filter(function() {
-//         return $(this).text().toLowerCase().indexOf(searchText) === -1;
-//       }).hide();
-//     });
-//   });
-
-
-// refactored to not use jQuery, and to use a timeout
-// to prevent the search from running on every keypress
 let searchTimeout = null;
 const searchInput = document.querySelector('#search');
 
 searchInput.addEventListener('keyup', ({ target }) => {
-
 	// This function will run after 500ms of no keypress
 	const runSearch = () => {
 		const searchText = target.value.toLowerCase();
